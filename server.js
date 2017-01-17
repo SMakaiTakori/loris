2f0dd99ce4ab0dfd1c3e8ba1f66b9f11336052bb
Perry Craft <LjCraft12@gmail.com>
2017-01-16 21:58:32 -0500

    Angular and Bootstrap connected along with custom style sheet

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app         = express();
 var port        = process.env.PORT || 3000;
 var mongoose    = require('mongoose');
 var morgan      = require('morgan');
-var bodyParser = require('body-parser');
-var User        = require('./app/models/user');
-
+var bodyParser  = require('body-parser');
+var router      = express.Router();
+var appRoutes   = require('./app/routes/api')(router);
+var path        = require('path');
 
 ///// Set up Morgan middleware /////
 app.use(morgan('dev'));
@@ -13,6 +14,12 @@ app.use(morgan('dev'));
 ///// Set up body parser middleware /////
 app.use(bodyParser.json());                                                     // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));                             // for parsing application/x-www-form-urlencoded
+
+///// Express static route /////
+app.use(express.static(__dirname + '/public'));
+
+///// https://localhost:3000/api/newuser
+app.use('/api', appRoutes);
 
 ///// Configure Mongo database /////
 mongoose.connect('mongodb://localhost:27017/loris_site', function (err) {
@@ -23,29 +30,11 @@ mongoose.connect('mongodb://localhost:27017/loris_site', function (err) {
     }
 });
 
-app.post('/newuser', function (req, res) {
-   var user       = new User();
-   user.firstName = req.body.firstName;
-   user.lastName  = req.body.lastName;
-   user.email     = req.body.email;
-   user.username  = req.body.username;
-   user.password  = req.body.password;
-    if (req.body.firstName == null || req.body.firstName == '' ||
-        req.body.lastName  == null || req.body.lastName  == '' ||
-        req.body.email     == null || req.body.email     == '' ||
-        req.body.username  == null || req.body.username  == '' ||
-        req.body.password  == null || req.body.password  == '') {
-        res.send('Ensure all fields marked with an asterisk were filled out')
-    } else {
-        user.save(function (err) {
-            if (err) {
-                res.send('Username or E-mail already exist!');
-            } else {
-                res.send('user created');
-            }
-        });
-    }
+///// Static index page /////
+app.get('*', function (req, res) {
+    res.sendFile((path.join(__dirname + '/public/app/views/index.html')));
 });
+
 ///// Listening for server connection on port 3000 /////
 app.listen(port, function () {
     console.log('Connected to Node server on port: ' + port + '!');
